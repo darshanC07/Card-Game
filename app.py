acaca5ef1948d73e4fe6dc7a0f48606887e8b6f9
Darshan <darshanchoudhary2007@gmail.com>
2025-01-12 16:07:33 +0530

    adding eventlet server

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from random import randint
 import json
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
 cards = cards
 
